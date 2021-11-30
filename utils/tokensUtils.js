8d23b81b298675b6d0b9d2e311d542defb270ebf
johiny <johiny7@live.com>
2021-11-30 15:25:38 -0500

    llave privada del token agregada a las variables de entorno

--- utils/tokensUtils.js
+++ utils/tokensUtils.js
@@ -1,13 +1,13 @@
 import  jwt  from "jsonwebtoken"
 
 const generateToken = (payload) =>{
-    return jwt.sign(payload,"IlovePizza",{expiresIn: "48h"})
+    return jwt.sign(payload, process.env.PRIVATE_KEY_JWT,{expiresIn: "48h"})
 };
 
 const validateToken = (token) =>{
     if(token)
     {
-        const verification = jwt.verify(token,"IlovePizza", (err,data) => {
+        const verification = jwt.verify(token,process.env.PRIVATE_KEY_JWT, (err,data) => {
             if(err)
             {
                 return {
